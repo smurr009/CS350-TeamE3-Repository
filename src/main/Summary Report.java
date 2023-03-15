53312285e31452b20f9899f1403816b98ff7fa37
kobe <klee028@gmail.com>
2023-03-15 19:31:32 -0400

    y

---
+++ src/main/Summary Report.java
@@ -0,0 +1,8 @@
+package main;
+public class Summary_Report {
+    
+    int percentage;
+    //string course;
+cout << percentage << "% of enrollment period has elapsed.";
+cout << "Course Enrollment Projected Cap";
+}
