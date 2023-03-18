6129fa359984d93d71cf0435be0888c9f45218b0
James Powers <jpowe007@odu.edu>
2023-03-17 20:10:26 -0400

    fixed unit testing

--- project/src/test/java/testSummaryReport.java
+++ project/src/test/java/testSummaryReport.java
@@ -3,9 +3,10 @@ package edu.odu.cs.cs350.pne;
 import java.io.File;
 import java.io.FileOutputStream;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat; 
 import static org.hamcrest.Matchers.*;
 
 import edu.odu.cs.cs350.pne.Summary_Report;
@@ -14,6 +15,6 @@ public class testSummaryReport {
     @Test
     public void testSummaryReportBasic()
     {
-
+        assertThat("dd", is(""));
     }
 }
