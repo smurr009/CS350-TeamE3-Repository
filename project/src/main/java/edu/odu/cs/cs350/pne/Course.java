8e1d59a01dc887c88dabb8bbb8ceeaed8c13c085
James Powers <jpowe007@odu.edu>
2023-03-17 21:24:54 -0400

    Course Class and Unit Test updates

--- project/src/main/java/edu/odu/cs/cs350/pne/Course.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Course.java
@@ -1,5 +1,26 @@
 package edu.odu.cs.cs350.pne;
 
+import java.lang.String;
+import java.time.LocalDate;
+
 public class Course {
-    
+    private String CourseName;
+    private LocalDate RegistrationDate;
+
+    public Course() {
+        CourseName = "";
+        RegistrationDate = LocalDate.now();
+    }
+
+    public Course(String CName, LocalDate RegDate) {
+        CourseName = CName;
+        RegistrationDate = RegDate;
+    }
+
+    public String getCourseName() { return CourseName; }
+    public LocalDate getRegistrationDate() { return RegistrationDate; }
+
+    public void setCourseName(String CName) { CourseName = CName; }
+    public void setRegistrationDate(LocalDate RegDate) { RegistrationDate = RegDate; }
+
 }
