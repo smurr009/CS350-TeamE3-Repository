f9e64292d446983b99c3137bc59c2026728139e7
James Powers <jpowe007@odu.edu>
2023-03-18 16:19:20 -0400

    updated Section class

--- project/src/main/java/edu/odu/cs/cs350/pne/Section.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Section.java
@@ -14,4 +14,24 @@ public class Section extends Course {
         EnrollmentCap = 0;
         Enrolled = 0;
     }
+
+    public Section(String _Subject, String _CourseNumber, String _CRN) {
+        super(_Subject, _CourseNumber);
+        CRN = _CRN;
+    }
+
+    public String getCRN() { return CRN; }
+    public int getEnrollmentCap() { return EnrollmentCap; }
+    public int getEnrolled() { return Enrolled; }
+
+    public void setCRN(String _CRN) { CRN = _CRN; }
+    public void setEnrollmentCap(int _EnrollmentCap) { EnrollmentCap = _EnrollmentCap; }
+    public void setEnrolled(int _Enrolled) {
+        if(_Enrolled <= EnrollmentCap) Enrolled = _Enrolled;
+    }
+
+    public void addToEnrollment(int numStudents) {
+        int openSeats = EnrollmentCap - Enrolled;
+        if(numStudents <= openSeats) Enrolled = Enrolled + numStudents;
+    }
 }
--- project/src/test/java/edu/odu/cs/cs350/pne/testSection.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/testSection.java
@@ -14,6 +14,21 @@ public class testSection {
         assertThat(s.getSubect(), is(""));
         assertThat(s.getCourseNumber(), is(""));
         assertThat(s.getCourseName(), is(""));
+        assertThat(s.getCRN(), is(""));
+        assertThat(s.getEnrollmentCap(), is(0));
+        assertThat(s.getEnrolled(), is(0));
+    }
+
+    @Test
+    public void testNonDefaultConstructor() {
+        // Testing NonDefault Constructor
+        Section s = new Section("CS", "350", "123456");
+        assertThat(s.getSubect(), is("CS"));
+        assertThat(s.getCourseNumber(), is("350"));
+        assertThat(s.getCourseName(), is("CS350"));
+        assertThat(s.getCRN(), is("123456"));
+        assertThat(s.getEnrollmentCap(), is(0));
+        assertThat(s.getEnrolled(), is(0));
     }
 
     @Test
@@ -23,11 +38,54 @@ public class testSection {
         assertThat(s.getSubect(), is(""));
         assertThat(s.getCourseNumber(), is(""));
         assertThat(s.getCourseName(), is(""));
+        assertThat(s.getCRN(), is(""));
+        assertThat(s.getEnrollmentCap(), is(0));
+        assertThat(s.getEnrolled(), is(0));
         // Apply Mutators
         s.setSubject("CS");
         s.setCourseNumber("350");
+        s.setCRN("987654");
+        s.setEnrollmentCap(15);
+        s.setEnrolled(10);
         assertThat(s.getSubect(), is("CS"));
         assertThat(s.getCourseNumber(), is("350"));
         assertThat(s.getCourseName(), is("CS350"));
+        assertThat(s.getCRN(), is("987654"));
+        assertThat(s.getEnrollmentCap(), is(15));
+        assertThat(s.getEnrolled(), is(10));
+        s.setEnrolled(16); // Try adding too many
+        assertThat(s.getEnrolled(), is(10)); // Should not change
+    }
+
+    @Test
+    public void testAddingEnrollment() {
+        // Testing NonDefault Constructor
+        Section s = new Section("CS", "350", "123456");
+        assertThat(s.getSubect(), is("CS"));
+        assertThat(s.getCourseNumber(), is("350"));
+        assertThat(s.getCourseName(), is("CS350"));
+        assertThat(s.getCRN(), is("123456"));
+        assertThat(s.getEnrollmentCap(), is(0));
+        assertThat(s.getEnrolled(), is(0));
+        // Setup Current Enrolmlent
+        s.setEnrollmentCap(25);
+        s.setEnrolled(15);
+        assertThat(s.getEnrollmentCap(), is(25));
+        assertThat(s.getEnrolled(), is(15));
+        // Add More Students
+        s.addToEnrollment(5);
+        assertThat(s.getEnrolled(), is(20));
+        // Add Too Many Students
+        s.addToEnrollment(9);
+        assertThat(s.getEnrolled(), is(20)); // Did not change
+    }
+
+    @Test
+    public void testEquality() {
+        Section s1 = new Section("CS", "350", "123456");
+        Section s2 = s1;
+        Section s3 = new Section("CS", "350", "987651");
+        assertThat(s1, equalTo(s2));
+        assertThat(s1, not(equalTo(s3)));
     }
 }
