3ad601e9699b86838fb81ae8ca46640f7f2271d8
cs_smurr009 <cs_smurr009@odu.edu>
2023-03-17 20:44:26 -0400

    basic constructor of SemestersClass

--- project/src/main/java/edu/odu/cs/cs350/pne/Semester.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Semester.java
@@ -1,5 +1,17 @@
 package edu.odu.cs.cs350.pne;
 
+import java.time.LocalDate;
+
 public class Semester {
     
+    int semesterCode; 
+    LocalDate startDate;
+    LocalDate endDate;
+
+    /* creation of each semester directory should have a defined start date, end date, and semesterCode */
+    public Semester(LocalDate start, LocalDate end, int semester) {
+        this.startDate = start;
+        this.endDate = end;
+        this.semesterCode = semester;
+    }
 }
