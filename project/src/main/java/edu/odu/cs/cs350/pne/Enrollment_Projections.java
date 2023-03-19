c14000ae14d75628f00d3eeb3ae0f0543efcc945
Alexji Gaston <agast002@odu.edu>
2023-03-18 23:49:45 -0400

    'Adding things'

--- project/src/main/java/edu/odu/cs/cs350/pne/Enrollment_Projections.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Enrollment_Projections.java
@@ -4,10 +4,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
+//I have to fix this later to make sure it runs with the program's requirements
 public class Enrollment_Projections {
     public String CommandLineParameters(String[] args)
     {
+        
+        //Code to add later on and fix for the unit test
+        /**
+         * May need a Java version of an initializer for the list of semester directories
+         * ArrayList<Integer> semesterDirectories = new ArrayList();
+         * Check to see if the parameters have a date format at the end
+         * Check to see the paramters include a list of semester directories
+         * Check to see if the parameters are in order? If else statements?
+         * Sample code below from Steven Zeil at:
+         * https://www.cs.odu.edu/~zeil/cs350/polawar/Protected/enrollmentProjectionDesign/index.html
+         * int i = 0;
+		    for (String clParam: args) {
+			    System.out.println("CLI param " + i + " is: " + clParam);
+			    ++i;
+		    }
+         */
         String response = "";
         //If there is 0
         if (args.length == 0)
--- project/src/main/java/edu/odu/cs/cs350/pne/Enrollment_Snapshots.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Enrollment_Snapshots.java
@@ -2,5 +2,20 @@ package edu.odu.cs.cs350.pne;
 
 public class Enrollment_Snapshots
 {
+    /**
+     * This function filters out snapshots prior
+     * to the start of pre-registration
+     */
+    public void FilterPreRegistration()
+    {
+
+    }
     
+    /**
+     * This function filters out snapshots taken before
+     * the first day of add deadline
+     */
+    public void FilterPostAddDeadline(){
+
+    }
 }
