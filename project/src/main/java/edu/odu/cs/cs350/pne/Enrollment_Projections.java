1ec7ebcbc89ed260c7b2bf686657918ac3a5a8c4
Alexji Gaston <agast002@odu.edu>
2023-03-23 16:57:36 -0400

    'Little changes'

--- project/src/main/java/edu/odu/cs/cs350/pne/Enrollment_Projections.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Enrollment_Projections.java
@@ -7,54 +7,26 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 //I have to fix this later to make sure it runs with the program's requirements
+//I'm going to use an ArrayList to use
 public class Enrollment_Projections {
-    public String CommandLineParameters(String[] args)
+    public void CommandLineParameters(String[] args) throws IOException
     {
-        
-        //Code to add later on and fix for the unit test
-        /**
-         * May need a Java version of an initializer for the list of semester directories
-         * ArrayList<Integer> semesterDirectories = new ArrayList();
-         * Check to see if the parameters have a date format at the end
-         * Check to see the paramters include a list of semester directories
-         * Check to see if the parameters are in order? If else statements?
-         * Sample code below from Steven Zeil at:
-         * https://www.cs.odu.edu/~zeil/cs350/polawar/Protected/enrollmentProjectionDesign/index.html
-         * int i = 0;
-		    for (String clParam: args) {
-			    System.out.println("CLI param " + i + " is: " + clParam);
-			    ++i;
-		    }
-         */
-        String response = "";
-        //If there is 0
-        if (args.length == 0)
-        {   
-            response = "Nothing is provided. Cannot run the projections...";
-            System.out.println(response);
-        }
-        //If there is 1
-        else if(args.length < 2)
+        boolean semesterDirectoryhistorical = false;
+        boolean semesterDirectoryLocation = false;
+        //This doesn't take into consideration the last date option
+        if(args.length < 4)
         {
-            response = "Historical enrollment is provided. Directory location is still needed...";
-            System.out.println(response);
-        }
-        //If there is 2
-        else if(args.length < 3)
-        {
-            response = "Historical enrollment is provided.";
-            response += "Direction location is provided.";
-            response += "However, a path to write the report is still needed...";
-            System.out.println(response);
-        }
-        else
-        {
-            for(int i = 0; i < args.length; i ++)
+            for(int i = 0; i < args.length;i++)
             {
-                response += args[i];
-                System.out.println(args[i]);
+                if(args[i].contains("2022"))
+                {
+
+                }
             }
+            
         }
-        return response;
+        //I need to take into consideration the last date option and the list of semester directories
+        
+
     }
 }
--- project/src/test/java/edu/odu/cs/cs350/pne/testEnrollmentProjections.java
+++ project/src/test/java/edu/odu/cs/cs350/pne/testEnrollmentProjections.java
@@ -11,85 +11,13 @@ import edu.odu.cs.cs350.pne.Enrollment_Projections;
 public class testEnrollmentProjections
 {
     @Test
-    public void testCommandLineParamatersNoLine()
+    public void testCommandLineParamaters()
     {
-        String nullPointer = "";
-        String verify = "";
-        String verify1 = "";
-        Enrollment_Projections test = new Enrollment_Projections();
-        Enrollment_Projections test2 = new Enrollment_Projections();
-        try {
-            String args[] = {};
-            verify = test.CommandLineParameters(args);
-            System.out.println(verify);
-            verify1 = test2.CommandLineParameters(args);
-		}
-		catch(NullPointerException e)
-        {
-            nullPointer = "NullPointerException thrown!";
-			System.out.println(nullPointer);
-		}
-        
-        assertFalse(verify.equals(nullPointer));
-        assertTrue(verify.equals(verify1));
-    }
-    @Test 
-    public void testCommandLineParamatersOneLine()
-    {
-        Enrollment_Projections test = new Enrollment_Projections();
-        String testArray[] = {"Historical enrollment"};
-        String verify1 = test.CommandLineParameters(testArray);
-        Enrollment_Projections test2 = new Enrollment_Projections();
-        String Arr[] = {};
-        String verify2 = test2.CommandLineParameters(Arr);
-        assertFalse(verify1.equals(verify2));
-        
-        String testerArray[] = {"Historical Enrollment"};
-        verify2 = test2.CommandLineParameters(testerArray);
-        assertTrue(verify2.equals(verify1));
-    }
-    @Test 
-    public void testCommandLineParamatersTwoLines()
-    {
-        Enrollment_Projections test = new Enrollment_Projections();
-        String testArr1[] = {"Historical enrollment", "Semester directory"};
-        String verify1 = test.CommandLineParameters(testArr1);
-        Enrollment_Projections test2 = new Enrollment_Projections();
-        String Arr[] = {};
-        String verify2 = test2.CommandLineParameters(Arr);
-        assertFalse(verify1.equals(verify2));
-        
-        String testArr2[] = {"Historical Enrollment"};
-        verify2 = test2.CommandLineParameters(testArr2);
-        assertFalse(verify2.equals(verify1));
 
-        String testArr3[] = {"Historical Enrollment", "Another semester directory"};
-        verify2 = test2.CommandLineParameters(testArr3);
-        assertTrue(verify2.equals(verify1));
     }
     @Test
-    public void testCommandLineParametersThreeLines()
+    public void testCommandLineParamatersNoLine()
     {
-        Enrollment_Projections test = new Enrollment_Projections();
-        String testArr1[] = {"Historical enrollment", "Semester directory", "path"};
-        String verify1 = test.CommandLineParameters(testArr1);
-        Enrollment_Projections test2 = new Enrollment_Projections();
-        String Arr[] = {};
-        String verify2 = test2.CommandLineParameters(Arr);
-        assertFalse(verify1.equals(verify2));
-        
-        String testArr2[] = {"Historical Enrollment"};
-        verify2 = test2.CommandLineParameters(testArr2);
-        assertFalse(verify2.equals(verify1));
 
-        String testArr3[] = {"Historical Enrollment", "Another semester directory"};
-        verify2 = test2.CommandLineParameters(testArr3);
-        assertFalse(verify2.equals(verify1));
-
-        String testArr4[] = {"Historical Enrollment", "Another semester directory", "path"};
-        verify2 = test2.CommandLineParameters(testArr4);
-        System.out.println(verify2);
-        assertFalse(verify1.equals(verify2));
-        
     }
 }
