51e0ec21033fb18e2ed7aa8745c11d969f9ec22c
cs_smurr009 <cs_smurr009@odu.edu>
2023-03-25 20:50:15 -0400

    command line creation

---
+++ project/src/main/java/edu/odu/cs/cs350/pne/commandLine.java
@@ -0,0 +1,8 @@
+package edu.odu.cs.cs350.pne;
+
+public class commandLine {
+    
+    
+
+
+}
---
+++ project/src/test/java/edu/odu/cs/cs350/pne/testCommandLine.java
@@ -0,0 +1,24 @@
+package edu.odu.cs.cs350.pne;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat; 
+import static org.hamcrest.Matchers.*;
+
+import edu.odu.cs.cs350.pne.Detailed_Report;
+
+public class testCommandLine {
+    
+
+    @Test 
+    public void testInputParameters()
+    {
+        inputParameters test = new inputParameters();
+        
+    }
+
+}
