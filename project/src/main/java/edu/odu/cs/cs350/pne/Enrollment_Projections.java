8fbecce5a6076ab4fff706d289ec1746efac7447
Alexji Gaston <agast002@odu.edu>
2023-04-03 16:34:43 -0400

    Changing functions and parameters

--- project/src/main/java/edu/odu/cs/cs350/pne/Enrollment_Projections.java
+++ project/src/main/java/edu/odu/cs/cs350/pne/Enrollment_Projections.java
@@ -1,24 +1,50 @@
 package edu.odu.cs.cs350.pne;
 
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.time.LocalDate;
-import com.opencsv.CSVReader;
-import com.opencsv.CSVReaderBuilder;
-import com.opencsv.exceptions.CsvValidationException;
-import java.io.InputStreamReader;
 
-//Needs to 
+//Needs to get data from EnrollmentSpanshots
 public class Enrollment_Projections {
-    LocalDate start;
-    LocalDate end;
-    public void Enrollment_Projection_Formula()
-    {
+    LocalDate currentEndDate;
+    LocalDate historicalEndDate;
 
+    /**
+     * The default Constructor
+     */
+    public Enrollment_Projections()
+    {
+        currentEndDate = LocalDate.now();
+        historicalEndDate =  LocalDate.now();
     }
-
-    public void Interpolation()
+    
+    /**
+     * Create a new Enrollment_Projection
+     * @param ceDate
+     * @param heDate
+     */
+    public Enrollment_Projections(LocalDate ceDate, LocalDate heDate)
     {
-
+        currentEndDate = ceDate;
+        historicalEndDate = heDate;
+    }
+    /**
+     * Calculates the projection for the future semester with data from the current and historical snapshots
+     * @param cDate
+     * @param hDate
+     */
+    public void Enrollment_Projection_Formula(LocalDate cDate, LocalDate hDate)
+    {
+        
+    }
+    
+    /**
+     * Calculates the interpolation between the current and historical snapshots
+     * @param cDate
+     * @param haDate
+     * @param hbDate
+     */
+    public void Interpolation(LocalDate cDate, LocalDate haDate, LocalDate hbDate)
+    {
+        //Current and historical enrollments as well (I don't understand the formula all that well)
+        //Fill in later
     }
 }
